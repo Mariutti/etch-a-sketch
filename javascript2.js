aef7dc7d8c5fb8bea6700c3ab1203fd69016c114
Mariutti <pedromariutti@gmail.com>
2023-01-04 21:36:23 -0300

    Adjust the alternative version

--- javascript2.js
+++ javascript2.js
@@ -6,9 +6,24 @@ const counter = document.querySelector('#slider');
 
 paraCounter.textContent = `${counter.value} X ${counter.value}`
 
+const container = document.querySelector('.container');
 counter.oninput = function(){
     paraCounter.textContent = `${this.value} X ${this.value}`;
 }
 
+const divContainer = document.querySelector('.container');
 
-const divContainer = document.querySelector('.container')
+
+const gridBtn = document.querySelector('#gridBtn');
+gridBtn.addEventListener('click', createDivs);
+
+function createDivs(){
+    divContainer.innerHTML = '';
+    for(let i = 1; i <= Math.pow(counter.value, 2); i++){
+        const divGrid = document.createElement('div');
+        divGrid.className = 'divGrid';
+        divContainer.appendChild(divGrid);
+        divGrid.style.height = `${800/counter.value}px`;
+        divGrid.style.width = `${800/counter.value}px`;
+    }
+}
